bf087b5e0a18153a225969ca96435ae2b43b1cc3
alkeity <alkeity.23@gmail.com>
2025-03-21 04:59:51 +0300

    hw

--- JavaScript/03. Events/events-hw.js
+++ JavaScript/03. Events/events-hw.js
@@ -16,15 +16,15 @@ function convertBinaryToDecimal()
 }
 
 var targetDate;
-var countdownID;
+var countdownID = null;
 
 function countdownTimer()
 {
-    targetDate = document.getElementById('targetDate').value;
+    let targetDateElem = document.getElementById('targetDate');
 
     let curDate = new Date();
-    targetDate = new Date(targetDate);
-    targetDate.setHours(targetDate.getHours() + Math.floor(curDate.getTimezoneOffset() / 60));
+    targetDate = new Date(targetDateElem.value);
+    //targetDate.setHours(targetDate.getHours() + Math.floor(curDate.getTimezoneOffset() / 60));
     //targetDate = new Date(targetDate.toLocaleString("default", {timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone}));
     //targetDate = new Date(targetDate + curDate.getTimezoneOffset() * 60000);
 
@@ -36,6 +36,8 @@ function countdownTimer()
     if (targetDate > curDate)
     {
         // targetDate = Date.UTC(targetDate.getFullYear(), targetDate.getMonth(), targetDate.getDate());
+        targetDateElem.disabled = true;
+        document.getElementById('btnStart').disabled = true;
         countdownTick();
     }
     else
@@ -55,69 +57,74 @@ function countdownTick()
     //                 );
     let seconds = Math.floor((targetDate - curDate) / 1000);
 
-    if (seconds > 0)
+    let years = 0;
+    let months = 0;
+    let days = 0;
+    let hours = 0;
+    let minutes = 0;
+
+    if (seconds >= (60 * 60 * 24 * 30 * 12))
     {
-        let years = 0;
-        let months = 0;
-        let days = 0;
-        let hours = 0;
-        let minutes = 0;
-
-        if (seconds >= (60 * 60 * 24 * 30 * 12))
-        {
-            years = Math.floor(seconds / (60 * 60 * 24 * 30 * 12));
-            seconds -= years * (60 * 60 * 24 * 30 * 12);
-        }
-
-        if (seconds >= (60 * 60 * 24 * 30))
-        {
-            months = Math.floor(seconds / (60 * 60 * 24 * 30));
-            seconds -= months * (60 * 60 * 24 * 30);
-        }
-
-        if (seconds >= (60 * 60 * 24))
-        {
-            days = Math.floor(seconds / (60 * 60 * 24));
-            seconds -= days * (60 * 60 * 24);
-        }
-
-        if (seconds >= (60 * 60))
-        {
-            hours = Math.floor(seconds / (60 * 60));
-            seconds -= hours * (60 * 60);
-        }
-
-        if (seconds >= 60)
-        {
-            minutes = Math.floor(seconds / 60);
-            seconds -= minutes * 60;
-        }
-
-        let resultStr = "";
-
-        if (years != 0)
-        {
-            resultStr += `<p>Years: ${years}</p>`;
-        }
-
-        if (months != 0)
-        {
-            resultStr += `<p>Months: ${months}</p>`;
-        }
-
-        if (days != 0)
-        {
-            resultStr += `<p>Days: ${days}</p>`;
-        }
-        
-        resultStr += `<p>Hours: ${hours}</p><p>Minutes: ${minutes}</p><p>Seconds: ${seconds}</p>`;
-
-        document.getElementById('resultCountdown').innerHTML = resultStr;
-
-        countdownID = setTimeout(countdownTick, 1000);
+        years = Math.floor(seconds / (60 * 60 * 24 * 30 * 12));
+        seconds -= years * (60 * 60 * 24 * 30 * 12);
     }
-    else
+
+    if (seconds >= (60 * 60 * 24 * 30))
+    {
+        months = Math.floor(seconds / (60 * 60 * 24 * 30));
+        seconds -= months * (60 * 60 * 24 * 30);
+    }
+
+    if (seconds >= (60 * 60 * 24))
+    {
+        days = Math.floor(seconds / (60 * 60 * 24));
+        seconds -= days * (60 * 60 * 24);
+    }
+
+    if (seconds >= (60 * 60))
+    {
+        hours = Math.floor(seconds / (60 * 60));
+        seconds -= hours * (60 * 60);
+    }
+
+    if (seconds >= 60)
+    {
+        minutes = Math.floor(seconds / 60);
+        seconds -= minutes * 60;
+    }
+
+    let resultStr = "";
+
+    if (years != 0)
+    {
+        resultStr += `<p>Years: ${years}</p>`;
+    }
+
+    if (months != 0)
+    {
+        resultStr += `<p>Months: ${months}</p>`;
+    }
+
+    if (days != 0)
+    {
+        resultStr += `<p>Days: ${days}</p>`;
+    }
+    
+    resultStr += `<p>Hours: ${hours}</p><p>Minutes: ${minutes}</p><p>Seconds: ${seconds}</p>`;
+
+    document.getElementById('resultCountdown').innerHTML = resultStr;
+
+    countdownID = setTimeout(countdownTick, 1000);
+}
+
+function countdownStop()
+{
+    if (countdownID)
     {
         clearTimeout(countdownID);
+        countdownID = null;
+        document.getElementById('resultCountdown').innerHTML = "";
+        document.getElementById('btnStart').disabled = false;
+        document.getElementById('targetDate').disabled = false;
     }
 }
