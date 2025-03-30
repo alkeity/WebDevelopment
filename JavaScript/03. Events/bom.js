29f260dc22bb3acc75e79b7bb12eb1a12426af6e
alkeity <alkeity.23@gmail.com>
2025-03-31 02:39:10 +0300

    classwork

--- JavaScript/03. Events/bom.js
+++ JavaScript/03. Events/bom.js
@@ -5,3 +5,17 @@ function screenResolution()
 }
 
 screenResolution();
+
+function testObj()
+{
+    let test = new Object();
+
+    test.width = 100;
+    test.height = 200;
+    console.log(test);
+    delete test.height;
+    console.log(test);
+    test.items = 3;
+    console.log(test);
+    console.log(Object.keys(test));
+}
